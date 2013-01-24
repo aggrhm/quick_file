fad1d088cd72269fdb399a5f756b491ef56b681f
Alan Graham <alan@productlab.com>
2013-01-24 00:10:05 -0500

    added right_aws

--- lib/quick_file/storage.rb
+++ lib/quick_file/storage.rb
@@ -1,4 +1,5 @@
 require 'aws-sdk'
+require 'right_aws'
 
 module QuickFile
   class Storage
@@ -10,15 +11,20 @@ module QuickFile
 
       if @provider == :local
         # prepare for local storage access
-      else
+      elsif @provider == :s3
         conn_opts = {
           :s3_endpoint       => @options[:host],
-          :s3_port       => ( @options[:use_ssl] ? 443 : 80 ),
+          :use_ssl       => ( @options[:use_ssl].nil? ? false : @options[:use_ssl] ),
           :access_key_id     => @options[:access_key_id],
           :secret_access_key => @options[:secret_access_key]
         }
         #conn_opts[:use_ssl] = @options[:use_ssl] if @options[:use_ssl]
         @interface = AWS::S3.new(conn_opts)
+      elsif @provider == :ceph
+        use_ssl = @options[:use_ssl] || false
+        port = use_ssl ? 443 : 80
+        proto = use_ssl ? 'https' : 'http'
+        @interface = RightAws::S3.new(@options[:access_key_id], @options[:secret_access_key], {:server => @options[:host], :port => port, :protocol => proto, :no_subdomains => true})
       end
             
     end
@@ -34,17 +40,19 @@ module QuickFile
     def set_bucket(bucket)
       @bucket_name = bucket
 
-      if is_provider? [:ceph, :s3]
+      if is_provider? [:s3]
         @bucket = @interface.buckets[@bucket_name] || @interface.buckets.create(@bucket_name)
+      elsif is_provider? [:ceph]
+        @bucket = @interface.bucket(@bucket_name) || @interface.bucket(@bucket_name, true)
       end
     end
 
     def store(opts)
-      if is_provider? [:ceph, :s3]
-
+      if is_provider? [:s3]
         #AWS::S3::S3Object.store(opts[:key], opts[:body], @bucket_name, :content_type => opts[:content_type])
         @bucket.objects[opts[:key]].write(opts[:body])
-
+      elsif is_provider? [:ceph]
+        @bucket.put(opts[:key], opts[:body])
       elsif is_provider? [:local]
 
         path = local_path(opts[:key])
@@ -55,20 +63,26 @@ module QuickFile
     end
 
     def delete(key)
-      if is_provider? [:ceph, :s3]
+      if is_provider? [:s3]
         #AWS::S3::S3Object.delete(key, @bucket_name)
         @bucket.objects[key].delete
+      elsif is_provider? [:ceph]
+        @bucket.key(key).delete
       elsif is_provider? [:local]
         File.delete(local_path(key))
       end
     end
 
     def download(key, to_file)
-      if is_provider? [:ceph, :s3]
+      if is_provider? [:s3]
         open(to_file, 'wb') do |file|
           @bucket.objects[key].read do |chunk|
             file.write(chunk)
           end
+        end
+      elsif is_provider? [:ceph]
+        open(to_file, 'wb') do |file|
+          file.write @bucket.get(key)
         end
       elsif is_provider? [:local]
         open(to_file, 'w') do |file|
@@ -78,9 +92,11 @@ module QuickFile
     end
 
     def value(key)
-      if is_provider? [:ceph, :s3]
+      if is_provider? [:s3]
         return @bucket.objects[key].read
         #return AWS::S3::S3Object.value(key, @bucket_name)
+      elsif is_provider? [:ceph]
+        return @bucket.get(key)
       elsif is_provider? [:local]
         File.open(local_path(key)).read
       end
--- quick_file.gemspec
+++ quick_file.gemspec
@@ -21,6 +21,7 @@ Gem::Specification.new do |s|
 	s.add_dependency 'rmagick'
 	s.add_dependency 'mime-types'
 	s.add_dependency 'aws-sdk'
+  s.add_dependency 'right_aws'
 
   # specify any dependencies here; for example:
   # s.add_development_dependency "rspec"
